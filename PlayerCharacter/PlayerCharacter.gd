c54098b0a6ba28162f0afc7bcec56462948ca587
ProximityParadox <84285785+ProximityParadox@users.noreply.github.com>
2024-03-25 22:03:11 +0100

    created box throwing system
    created box object
    added physics to object
    created pick up and throw methods
    
    you can now pick up and throw the rigid body object.

--- PlayerCharacter/PlayerCharacter.gd
+++ PlayerCharacter/PlayerCharacter.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+signal request_box_status
+
 #TODO has to be changed later to accomodate animations
 @onready var sprite_2d : Sprite2D = $Sprite2D
 
@@ -8,10 +10,22 @@ extends CharacterBody2D
 @export var jump_velocity : float = -430.0
 @export var friction : float = 0.8
 @export var max_fall_speed : float = 400
-
+@export var push_force: float = 150.0
+var picked_up_box : RigidBody2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var default_gravity : int = ProjectSettings.get_setting("physics/2d/default_gravity")
 var fast_fall_gravity : int = default_gravity * 1.5
+var looking_direction : float
+#func _ready():
+#	
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_E:
+			emit_signal("request_box_status")
+		if event.pressed and event.keycode == KEY_T:
+			if picked_up_box != null:
+				picked_up_box.throw(self)
 
 
 #TODO break out into more funcs
@@ -25,8 +39,11 @@ func _physics_process(delta):
 		# Ensure fall speed past max_fall_speed is consistent
 		else:
 			velocity.y = max_fall_speed
-		
-		
+
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i)
+		if c.get_collider() is RigidBody2D:
+			c.get_collider().apply_central_impulse(-c.get_normal()*push_force)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -35,15 +52,27 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction : float = Input.get_axis("ui_left", "ui_right")
+	if direction != 0:
+		looking_direction = direction
 	# Check to make sure player doesn't slide more when running opposite way
 	# There may be a better solution
 	if (direction == 1 and velocity.x >= 0) or (direction == -1 and velocity.x <= 0):
 		velocity.x = move_toward(velocity.x, direction * speed, (speed * 5) * acceleration * delta)
 		if direction > 0:
+			$Area2D.position.x = 41
 			sprite_2d.flip_h = false;
 		else:
 			sprite_2d.flip_h = true;
+			$Area2D.position.x = -41
 	else:
 		velocity.x = move_toward(velocity.x, 0, (speed * 10) * friction * delta)
 
 	move_and_slide()
+
+
+func _on_area_2d_send_box_status(arg2):
+	for body in arg2:
+		if body.name == "pick_up_box":
+			body.pick_up(self)
+			picked_up_box = body
+
