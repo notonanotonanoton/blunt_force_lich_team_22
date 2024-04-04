02ef450b2773f58bab7706830353ab9ecc7f91df
holmbeerg <mansholmberg1@gmail.com>
2024-04-04 21:32:07 +0200

    remove unnecessary code

--- States/PlayerCarryingBox.gd
+++ States/PlayerCarryingBox.gd
@@ -14,10 +14,7 @@ func update(_delta: float):
 	if Input.is_key_pressed(KEY_T) and player_character.is_on_floor():
 		if picked_up_box != null:
 			picked_up_box.thrown_by(player_character)
-			if (player_character.is_on_floor()):
-				state_transition.emit(self, "playeronground")
-			else:
-				state_transition.emit(self, "playerinair")
+			state_transition.emit(self, "playeronground")
 			
 func _on_area_2d_send_box_status(arg2):
 	for body in arg2:
