73294e81125a7c797b8ab8b0a9232374a17cf18b
ProximityParadox <84285785+ProximityParadox@users.noreply.github.com>
2024-04-14 16:07:30 +0200

    Update HurtBoxScript.gd

--- healthanddamage/HurtBoxScript.gd
+++ healthanddamage/HurtBoxScript.gd
@@ -3,7 +3,7 @@ class_name  hit_box_component
 
 @export var health_component : HealthComponent
 
-func damage(damage : int, enemy_position : Vector2):
+func damage(damage : int, enemy_position : Vector2) -> void:
 	if health_component:
 		health_component.take_damage(damage, enemy_position)
 
