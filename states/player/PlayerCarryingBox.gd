247e7c3bc5ea702ed16a7cabaeb9a2becb2453b3
holmbeerg <mansholmberg1@gmail.com>
2024-04-22 12:11:45 +0200

    first version of charging box throw mechanic, is there delay when throwing?

--- states/player/PlayerCarryingBox.gd
+++ states/player/PlayerCarryingBox.gd
@@ -1,22 +1,35 @@
 extends State
 @onready var player_character : CharacterBody2D = $"../.."
 var picked_up_box : CharacterBody2D
+var charge_time : float = 0.0
+var charging_throw : bool = false
 signal request_box
 
 func enter() -> void:
 	print("Entered PlayerCarryingBox state")
-
+	
 func exit() -> void:
 	pass
 
 func update(_delta: float) -> void:
-	#print("Håller lådan")
-	if Input.is_key_pressed(KEY_T) and player_character.is_on_floor():
+	if Input.is_action_just_pressed("throw_box"):
 		if picked_up_box != null:
-			picked_up_box.thrown_by(player_character)
-			state_transition.emit(self, "playeronground")
+			charging_throw = true
+	
+	if charging_throw:
+		charge_time += _delta
+			
+	if (Input.is_action_just_released("throw_box")):
+		picked_up_box.thrown_by(player_character, charge_time)
+		charge_time = 0.0
+		charging_throw = false
+		state_transition.emit(self, "playeronground")
+
 			
 func _on_area_2d_send_box_status(arg : ) -> void:
 	for body in arg:
 		if body.name == "pick_up_box":
 			picked_up_box = body
+
+func _on_timer_timeout():
+	pass # Replace with function body.
