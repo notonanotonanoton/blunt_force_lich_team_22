44a2a70d092d6843af45b305b689254489c10470
holmbeerg <mansholmberg1@gmail.com>
2024-05-09 22:51:21 +0200

    work in progress, but can at least press resume, exit and get into settings

--- pausemenu/PauseMenu.gd
+++ pausemenu/PauseMenu.gd
@@ -1,16 +1,18 @@
 extends Control
 
+signal entered_settings
+
 func _ready():
 	hide()
 
 func _process(delta):
 	pass
 
-func resume():
+func resume() -> void:
 	hide()
 	get_tree().paused = false
 	
-func pause():
+func pause() -> void:
 	show()
 	get_tree().paused = true 
 	
@@ -20,13 +22,21 @@ func _input(event):
 	elif Input.is_action_just_pressed("pause") and get_tree().paused:
 		resume()
 
-func _on_resume_pressed():
+func _on_resume_pressed() -> void:
 	hide()
 	resume()
 
-func _on_restart_pressed():
+func _on_restart_pressed() -> void:
 	resume()
 	get_tree().reload_current_scene()
 
-func _on_exit_pressed():
+func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+func _on_settings_pressed() -> void:
+	$MenuContainer.set_visible(false)
+	emit_signal("entered_settings")
+	
+func _on_settings_menu_back_pressed() -> void:
+	$MenuContainer.set_visible(true)
+	show()
---
+++ pausemenu/Settings.gd
@@ -0,0 +1,44 @@
+extends Control
+
+signal back_pressed
+
+@onready var window_button : OptionButton = $HBoxContainer/WindowMode
+
+const WINDOW_MODE_ARRAY : Array[String] = [
+	"Full-Screen",
+	"Window Mode",
+	"Borderless Window",
+	"Borderless Fullscreen"
+]
+
+func _ready():
+	add_window_mode_items()
+	hide()
+
+func add_window_mode_items() -> void:
+	for window_mode in WINDOW_MODE_ARRAY:
+		window_button.add_item(window_mode)
+
+func _on_window_mode_item_selected(index) -> void:
+	match index:
+		0: #Fullscreen
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		1: #Window
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		2: #Borderless Window
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+		3:
+			#Borderless Fullscreen
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+
+
+func _on_pause_menu_entered_settings() -> void:
+	show()
+
+func _on_back_pressed() -> void:
+	hide()
+	emit_signal("back_pressed")
