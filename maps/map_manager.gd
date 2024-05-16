23b2f094634f17931c112e694b5b908ee42d7536
DeadRed04 <adrian.fagergren@gmail.com>
2024-05-16 19:05:24 +0200

    First version of level 3 (soon to be level 2) created
    Also updated the old level one maps

--- maps/map_manager.gd
+++ maps/map_manager.gd
@@ -7,7 +7,7 @@ var current_scene_instance : Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var random_scene : String = level1_levels.pick_random()
-	var next_scene : PackedScene = load("res://maps/TutorialWorld.tscn") #current testing level update when want 
+	var next_scene : PackedScene = load("res://maps/Level3/Version1.tscn") #current testing level update when want 
 	current_scene_instance = next_scene.instantiate()
 	add_child(current_scene_instance)
 
