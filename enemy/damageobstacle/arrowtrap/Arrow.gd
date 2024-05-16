774180a241b6ecf35df6f19aa89e1c679916b8bf
ProximityParadox <84285785+ProximityParadox@users.noreply.github.com>
2024-05-16 21:07:28 +0200

    arrow fixing

--- enemy/damageobstacle/arrowtrap/Arrow.gd
+++ enemy/damageobstacle/arrowtrap/Arrow.gd
@@ -4,7 +4,7 @@ class_name Arrow
 @export var ground_collider : CollisionShape2D
 @export var gravity_timer : Timer
 
-var default_gravity : int = ProjectSettings.get_setting("physics/2d/default_gravity") * 2
+var default_gravity : int = ProjectSettings.get_setting("physics/2d/default_gravity") / 6
 var can_deal_damage : bool = true
 
 var movement : Vector2 = Vector2.ZERO
@@ -12,16 +12,19 @@ var movement : Vector2 = Vector2.ZERO
 func _ready() -> void:
 	#change wait time in GravityTimer node if desired.
 	#beware it affects all entities that use Arrow, such as arrow trap and skeleton archer
-	gravity_timer.start()
+	gravity_timer.start() 
+	#print("created arrow")
 
-func _physics_process(delta : float) -> void:
-	position += movement * delta
+func _physics_process(delta : float) -> void:	
+	global_position += movement * delta
+
 	if has_overlapping_bodies():
 		queue_free()
-	if gravity_timer.is_stopped():
-		position.y -= default_gravity
 
-func add_movement(mov : Vector2, pos : Vector2, rot : Vector2) -> void:
+	if gravity_timer.is_stopped():
+		movement.y += default_gravity*delta
+	rotation = global_position.direction_to(self.global_position+(movement*100)-Vector2(0, 0)).angle()
+	
+func add_movement(mov : Vector2, pos : Vector2) -> void:
 	movement = mov
 	global_position = pos
-	rotation = rot.angle()
--- enemy/damageobstacle/arrowtrap/arrow_trap.gd
+++ enemy/damageobstacle/arrowtrap/arrow_trap.gd
@@ -5,21 +5,33 @@ class_name spawner
 @export var timer : Timer
 @export var time_to_spawn_projectile : int = 4
 
+var South = [89, 91]
+var West = [179, 181]
+var North = [269, 271]
+
 func _ready():
 	timer.one_shot = true
 	timer.autostart = false
 	timer.wait_time = time_to_spawn_projectile
+	#print(global_position)
 	
 	#if you want to implement a custom condition do it here
 	timer.start()
-
-
+	
+	print("arrow trap rotation: ", rotation_degrees)
+	if rotation_degrees > South[0] and rotation_degrees < South[1]:
+		print("facing south")
+	if rotation_degrees > West[0] and rotation_degrees < West[1]:
+		print("facing West")
+	if rotation_degrees > North[0] and rotation_degrees < North[1]:
+		print("facing North")
 
 
 func _on_timer_timeout():
-	
+	#print("attempting to spawn arrow")
 	var object : Node2D = projectile_to_spawn.instantiate()
-	get_parent().add_child.call_deferred(object)
-	object.position = global_position
+	get_parent().add_child.call(object)
+	#object.monoDirectional_movement(Vector2(0, -20), global_position+Vector2(-20,0))
+	object.add_movement(Vector2(200, 0), global_position+Vector2(10,0))
 	timer.start()
 	
