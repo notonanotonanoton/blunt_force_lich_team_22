39cc64ae840d0c1cc8fac5b1442e641357952211
notonanotonanoton <anotona@protonmail.com>
2024-05-23 01:22:47 +0200

    first draft of AI fine-tuning. added skeleton warrior sprite
    there was a bug that had to be fixed by removing _physics_process from state machine and calling the method in Enemy's _physics_process instead; alongside moving move_and_slide() to the bottom.

--- states/StateMachine.gd
+++ states/StateMachine.gd
@@ -15,12 +15,13 @@ func _ready() -> void:
 	if inital_state:
 		inital_state.enter()
 		current_state = inital_state
-		
-func _process(delta : float) -> void:
+
+#unused
+func update(delta : float) -> void:
 	if current_state:
 		current_state.update(delta)
 	
-func _physics_process(delta : float) -> void:
+func physics_update(delta : float) -> void:
 	if current_state:
 		current_state.physics_update(delta)
 		
