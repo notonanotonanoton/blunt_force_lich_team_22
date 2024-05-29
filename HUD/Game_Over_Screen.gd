03e3817451cac2b68708d67387e5a1c3ddae64a7
holmbeerg <mansholmberg1@gmail.com>
2024-05-29 17:13:16 +0200

    fix for being able to exit on death

--- HUD/Game_Over_Screen.gd
+++ HUD/Game_Over_Screen.gd
@@ -10,15 +10,10 @@ func _ready() -> void:
 	visible = false;
 	player_character.player_death.connect(_on_player_character_player_death)
 
-func _on_exit_button_pressed() -> void:
-	#print("exit pressed")
-	get_tree().quit()
-
 func _on_retry_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://mainmenu/TestMainMenu.tscn")
+	get_tree().change_scene_to_file("res://mainmenu/MainMenu.tscn")
 
 func _on_player_character_player_death() -> void:	
-	print("reached game over screen")
 	visible = true
 
 func _on_retry_pressed():
